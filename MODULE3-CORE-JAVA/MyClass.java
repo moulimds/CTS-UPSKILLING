489861d516aa1f5f5901bd82a981aaff300b4c14
moulimds <moulidas106@gmail.com>
2025-05-25 21:39:25 +0530

    ex 39(Class)

---
+++ MODULE3-CORE-JAVA/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public void sayHello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
