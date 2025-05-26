1d221b40f31bcb185325025eb8ddbdd1baac3907
Mouli M <155385926+moulimds@users.noreply.github.com>
2025-05-26 10:48:50 +0530

    Create EX22.sql
    22. Duplicate Registrations Check
    Detect if a user has been registered more than once for the same event.

---
+++ MODULE2/EX22.sql
@@ -0,0 +1,11 @@
+SELECT 
+    u.full_name,
+    u.email,
+    e.title,
+    COUNT(r.registration_id) as registration_count
+FROM Users u
+JOIN Registrations r ON u.user_id = r.user_id
+JOIN Events e ON r.event_id = e.event_id
+GROUP BY u.user_id, e.event_id, u.full_name, u.email, e.title
+HAVING COUNT(r.registration_id) > 1
+ORDER BY registration_count DESC;
